3b6c94447b9b25d3f3f8d13be9f942a566753283
leonardoXLT <148663990+leonardoXLT@users.noreply.github.com>
2024-06-06 19:15:52 +1000

    update: express routes

--- src/app.js
+++ src/app.js
@@ -2,6 +2,23 @@
 
 const express = require("express");
 const serverInstance = express();
+const PokemonRouter = require("./routers/pokemonRoutes.js");
+
+
+// Raw JSON in body allowed
+serverInstance.use(express.json());
+// Form data in body allowed
+serverInstance.use(express.urlencoded({extended: true}));
+
+
+// Every route that begins with /pokemon gets passed to PokemonRouter
+serverInstance.use("/pokemon", PokemonRouter);
+
+
+
+
+
+
 
 serverInstance.get("/", (request, response) => {
 	console.log("Someone visited the homepage of the server");
@@ -13,11 +30,27 @@ serverInstance.get("/", (request, response) => {
 
 serverInstance.post("/", (request, response) => {
 
+	console.log(request.body);
+
 	response.json({
 		message:"Received data:",
 		requestData: request.body
 	})
-})
+});
+
+serverInstance.put("/", (request, response) => {
+	response.json({message:"Put request received"})
+});
+
+serverInstance.patch("/", (request, response) => {
+	response.json({message:"Patch request received"})
+});
+
+serverInstance.delete("/", (request, response) => {
+	response.json({message:"Delete request received"})
+});
+
+
 
 // Make the instance available for other files to use
 module.exports = serverInstance;
